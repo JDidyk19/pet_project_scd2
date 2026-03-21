831db1d62eb9af35fc6ba12e18cf4660e14214e5
Yurii Didyk <juran2201@gmail.com>
2026-03-21 10:26:53 +0200

    Create scd2_dimensional_user table script for docker

---
+++ scripts/init_db.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS scd2_dimensional_users (
+    id SERIAL PRIMARY KEY.
+    user_id UUID,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    birthday DATE,
+    email TEXT NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    ts_db TIMESTAMP,
+    actual_from TIMESTAMP,
+    actual_to TIMESTAMP,
+    is_actual BOOLEAN
+)
